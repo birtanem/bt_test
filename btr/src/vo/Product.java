b3fc56f5106cbfb0a30254cb9d0386b86ad0d48f
ITWILL <ITWILL@DESKTOP-6181D4M>
2020-05-18 19:49:16 +0900

    context 수정 product dto만듦

---
+++ btr/src/vo/Product.java
@@ -0,0 +1,50 @@
+package vo;
+
+public class Product {
+	
+	private int p_num;
+	private String p_name;
+	private String p_content;
+	private String p_image;
+	private int p_price;
+	private int p_amount;
+	
+	public int getP_num() {
+		return p_num;
+	}
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public String getP_content() {
+		return p_content;
+	}
+	public void setP_content(String p_content) {
+		this.p_content = p_content;
+	}
+	public String getP_image() {
+		return p_image;
+	}
+	public void setP_image(String p_image) {
+		this.p_image = p_image;
+	}
+	public int getP_price() {
+		return p_price;
+	}
+	public void setP_price(int p_price) {
+		this.p_price = p_price;
+	}
+	public int getP_amount() {
+		return p_amount;
+	}
+	public void setP_amount(int p_amount) {
+		this.p_amount = p_amount;
+	}
+	
+	
+}
