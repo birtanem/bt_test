fe9e724b2512a315ba06ca2646a81557a734d2bb
ITWILL <ITWILL@DESKTOP-6181D4M>
2020-05-26 16:43:56 +0900

    배하나 ; 상품목록, 수정삭제(고나리자)

---
+++ btr/src/product/svc/productUpdateService.java
@@ -0,0 +1,21 @@
+package product.svc;
+
+import static common.db.JdbcUtil.*;  
+import java.sql.Connection;
+
+import product.dao.ProductDAO;
+import product.vo.ProductBean;
+
+public class productUpdateService {
+
+	public boolean productUpdate(String p_num, int region_region_code, String p_category, String p_name, int p_price,
+			int p_amount) {
+		boolean isUpdate=false;
+		
+		
+		
+		return isUpdate;
+	}
+	
+	
+}
